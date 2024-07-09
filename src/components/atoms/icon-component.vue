cf020d6714a24bfa2636de5f56fdacc2ed992b48
Andres Cordova <aecordova@innovacion.gob.sv>
2024-07-09 10:14:18 -0600

    add: new icon-component component.

---
+++ src/components/atoms/icon-component.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+  icono: String,
+  altText: String,
+  svgClass: String
+})
+</script>
+<template>
+  <img :src="icono" :class="svgClass" :alt="altText">
+</template>
