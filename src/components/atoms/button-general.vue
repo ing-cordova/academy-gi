a9e13b224f92cc98f2a8ac9fa0cf82726ee0947d
Andres Cordova <aecordova@innovacion.gob.sv>
2024-07-10 08:56:16 -0600

    fix: the right calling of the monochrome pallet into the component.

--- src/components/atoms/button-general.vue
+++ src/components/atoms/button-general.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <button
-    class="flex items-center bg-white hover:bg-tertiary select-none text-monochrome-main border-2 border-monochrome-main rounded-xl text-md w-auto mt-2 mb-2 mr-2 p-2">
+    class="flex items-center bg-white hover:bg-monochrome-tertiary hover:text-white select-none text-monochrome-main border-2 border-monochrome-main rounded-xl text-md w-auto mt-2 mb-2 mr-2 p-2">
     <img :src="iconSrc" :alt="text" class="mr-2"> {{ text }}
   </button>
 </template>
