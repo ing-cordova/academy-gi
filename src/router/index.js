156e5982cd19175512a9d7bf1ed3ec97d2b79cb8
Andres Cordova <aecordova@innovacion.gob.sv>
2024-07-10 11:25:46 -0600

    update: routes directly from another file.

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DashboardPage from '@/components/pages/dashboard-page.vue'
 import LoginPage from '@/components/pages/login-page.vue'
+import privateRoutes from '@/router/routes-private.js'
+import SYSTEM_NAME from '@/utils/configuration-env.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: privateRoutes.LOGIN,
       name: 'Login',
       component: LoginPage
     },
     {
-      path: '/dashboard',
+      path: privateRoutes.DASHBOARD,
       name: 'Dashboard',
       component: DashboardPage
     },
     {
       path: '/:pathMatch(.*)*',
-      redirect: { name: 'Login' },
+      redirect: { name: 'Login' }
     }
   ]
+})
+
+router.afterEach(() => {
+  document.title = SYSTEM_NAME || 'Administrator'
 })
 
 export default router
