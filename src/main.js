9d1c7724bc3e0fa5b9d35ae11b5935ff6c919008
Andres Cordova <aecordova@innovacion.gob.sv>
2024-07-15 13:59:31 -0600

    add: right use of the prime vue theme.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +11,17 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue, {
+  // Default theme configuration
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'light-theme dark-theme',
+      cssLayer: false
+    }
+  }
+});
 app.use(router)
 
 app.mount('#app')
